e84fab3ad92452beda8a17587fa1866eceb80692
Alexis De Chiara <alexis.de-chiara@hotmail.fr>
2024-04-17 14:41:56 +0200

    Add configs for build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  routeRules: {
+    '/': {isr: true},
+    '/about': { isr: true },
+    '/legal-notice': { isr: true }
+  },
   modules: [
     '@nuxt/content',
     '@nuxt/ui',
